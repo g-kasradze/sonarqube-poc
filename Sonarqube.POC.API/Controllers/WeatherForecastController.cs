12ba269eb2d252874b8cb858c58ed49f4ee653c7
Guram Kasradze <116283588+g-kasradze@users.noreply.github.com>
2023-02-09 12:11:48 +0400

    Update WeatherForecastController.cs

--- Sonarqube.POC.API/Controllers/WeatherForecastController.cs
+++ Sonarqube.POC.API/Controllers/WeatherForecastController.cs
@@ -11,11 +11,6 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private static readonly string[] Summaries2 = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
     private readonly ILogger<WeatherForecastController> _logger;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
